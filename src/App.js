074064c78a86461bd65bd87d5eaef8a81df3a097
Matthew Manzo <matthew.manzo1@gmail.com>
2020-07-09 16:03:57 -0400

    updated csv file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import axios from "axios";
 import soccer from "./csvjson.json";
+import Sentiment from "sentiment";
+
+var sentiment = new Sentiment();
+var result = sentiment.analyze("Dogs are awesome.");
+console.dir(result);
 
 class App extends Component {
   async componentDidMount() {
     let res = await axios.get(
       "https://cors-anywhere.herokuapp.com/https://horoscope-api.herokuapp.com/horoscope/year/libra"
     );
+
     console.log(res);
+    console.log(res.data.horoscope);
+    let horoscope = sentiment.analyze(res.data.horoscope);
+    console.log(horoscope);
     console.log(soccer);
   }
   render() {
