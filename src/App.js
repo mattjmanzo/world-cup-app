7af17fc0c86414e80ec6cbd3cbb4e3daa8ca3be6
Matthew Manzo <matthew.manzo1@gmail.com>
2020-07-09 12:15:19 -0400

    updated started code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import soccer from "./csvjson.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  async componentDidMount() {
+    let res = await axios.get(
+      "https://cors-anywhere.herokuapp.com/https://horoscope-api.herokuapp.com/horoscope/year/libra"
+    );
+    console.log(res);
+    console.log(soccer);
+  }
+  render() {
+    return <div></div>;
+  }
 }
 
 export default App;
