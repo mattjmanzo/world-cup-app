79b3af60f0413bc71d9fba4978f5c020280adae1
Carlos Cesar Valle <carloscesarvalle@Carloss-iMac.local>
2020-07-16 15:24:49 -0400

    stadiums pics done

--- src/components/GameSim.jsx
+++ src/components/GameSim.jsx
@@ -179,9 +179,9 @@ class GameSim extends Component {
       );
     }
 
-    let theCloud = React.createElement("ul", { className: "cloud" }, cloud);
+    //let theCloud = React.createElement("ul", { className: "cloud" }, cloud);
 
-    team1.unshift(theCloud);
+    team1.unshift(<ul className="cloud">{cloud}</ul>);
 
     return team1;
   };
@@ -502,7 +502,11 @@ class GameSim extends Component {
   render() {
     // console.log(this.state);
     // console.log(this.props);
-
+    const team1AllData = this.state.display
+      ? this.displayTeam(this.state.selectedTeam1)
+      : "";
+    const team1Imgs = team1AllData.slice(1);
+    const wordCloud = team1AllData[0];
     return (
       <div>
         <header
@@ -625,11 +629,7 @@ class GameSim extends Component {
         </header>
         <div className="row">
           <h2>Team 1</h2>
-          <div className="cards">
-            {this.state.display
-              ? this.displayTeam(this.state.selectedTeam1)
-              : ""}
-          </div>
+          <div className="cards">{team1Imgs}</div>
         </div>
         <div className="row">
           <h2>Team 2</h2>
@@ -638,6 +638,7 @@ class GameSim extends Component {
               ? this.displayTeam(this.state.selectedTeam2)
               : ""}
           </div>
+          {wordCloud}
         </div>
         <div>{this.state.display ? this.stadiumWeatherPanel() : ""}</div>
         <div>{this.state.display ? this.displayFinalScore() : ""}</div>
